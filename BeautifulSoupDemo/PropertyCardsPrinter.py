9f40805fd9265eaf9fcc07e3e1057b644c6133fe
Shakshouk <liav.burger22@Gmail.com>
2023-06-21 12:06:45 +0300

    scrape function and fixes

--- BeautifulSoupDemo/PropertyCardsPrinter.py
+++ BeautifulSoupDemo/PropertyCardsPrinter.py
@@ -1,62 +1,101 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import requests
-from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# def printPropertyCards(propertyCards):
+#     for index, card in enumerate(propertyCards):
+#         name = card.find('div', {'data-testid': 'title'})
+#         address = card.find('span', {'data-testid': 'address'})
+#         distance = card.find('span', {'data-testid': 'distance'})
+#         numOfReviews = card.find('div', {'class': 'd8eab2cf7f c90c0a70d3 db63693c62'})
+#         extras = card.find('div', class_='d506630cf3')
+#         newToBooking = card.find('span', class_='e2f34d59b1')
+#         xNightsAndAdults = card.find('div', {'data-testid': 'price-for-x-nights'})
+#         price = card.find('span', {'data-testid': 'price-and-discounted-price'})
+#         taxesAndCharges = card.find('div', {'data-testid': 'taxes-and-charges'})
+#
+#         print(f'Hotel {index + 1}:\n\t Name: {name.text.strip()}')
+#         print(f'\t Address: {address.text.strip()}')
+#         print(f'\t Distance from Center: {distance.text.strip()}')
+#         if newToBooking and newToBooking.text == "New to Booking.com": print(f'\t New to Booking.com')
+#         print(f'\t Reviews: {numOfReviews.text}') if numOfReviews else print('\t Reviews: No reviews yet')
+#         print(f'\t Extras: {extras.text}') if extras else print('\t Extras: No extras')
+#         print(f'\t Nights and people: {xNightsAndAdults.text.strip()}') if xNightsAndAdults else print(
+#             '\t Nights and people: No info')
+#         print(f'\t Price: {price.text.strip()}') if price else print('\t Price: No price')
+#         print(f'\t Taxes and charges: {taxesAndCharges.text.strip()}') if taxesAndCharges else print(
+#             '\t Taxes and charges: No info')
+#         print("----------------------------------------------")
+#
+#
+# if __name__ == '__main__':
+#     url = 'https://www.booking.com/searchresults.html?ss=Berlin&ssne=Berlin&ssne_untouched=Berlin&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaGqIAQGYAQ64ARfIAQzYAQHoAQGIAgGoAgO4AoD2taIGwAIB0gIkYjUwNGU2YzctYmIxNS00NDEyLWIwZDMtMjYxN2Y4MDg4YmQ32AIF4AIB&sid=77a53ffe11c95ad4fb890587265110d0&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-1746443&dest_type=city&checkin=2023-05-31&checkout=2023-06-14&group_adults=2&no_rooms=1&group_children=0'
+#     url2 = 'https://www.booking.com/searchresults.html?ss=Berlin&ssne=Berlin&ssne_untouched=Berlin&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaGqIAQGYAQ64ARfIAQzYAQHoAQGIAgGoAgO4AoD2taIGwAIB0gIkYjUwNGU2YzctYmIxNS00NDEyLWIwZDMtMjYxN2Y4MDg4YmQ32AIF4AIB&sid=77a53ffe11c95ad4fb890587265110d0&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-1746443&dest_type=city&checkin=2023-05-31&checkout=2023-06-14&group_adults=2&no_rooms=1&group_children=0'
+#     # set up selenium driver
+#     driver = webdriver.Chrome()
+#     driver.get(url)
+#
+#     # set implicit wait time to 10 seconds
+#     driver.implicitly_wait(10)
+#
+#     # wait until the element with ID "my_element" is present
+#     element_present = EC.presence_of_element_located(
+#         (By.CLASS_NAME, "a826ba81c4"))  # the first class of the property card
+#     WebDriverWait(driver, timeout=10).until(element_present)
+#
+#     # now that the element is present, get the page source
+#     html_source = driver.page_source
+#
+#     # close the browser
+#     driver.quit()
+#
+#     # use BeautifulSoup to parse the page source and extract the data you need
+#     soup = BeautifulSoup(html_source, "html.parser")
+#
+#     source = requests.get(url)
+#     property_cards = soup.find_all('div', {'data-testid': 'property-card'})
+#
+#     printPropertyCards(property_cards)
 
 
-def printPropertyCards(propertyCards):
-    for index, card in enumerate(propertyCards):
-        name = card.find('div', {'data-testid': 'title'})
-        address = card.find('span', {'data-testid': 'address'})
-        distance = card.find('span', {'data-testid': 'distance'})
-        numOfReviews = card.find('div', {'class': 'd8eab2cf7f c90c0a70d3 db63693c62'})
-        extras = card.find('div', class_='d506630cf3')
-        newToBooking = card.find('span', class_='e2f34d59b1')
-        xNightsAndAdults = card.find('div', {'data-testid': 'price-for-x-nights'})
-        price = card.find('span', {'data-testid': 'price-and-discounted-price'})
-        taxesAndCharges = card.find('div', {'data-testid': 'taxes-and-charges'})
+from datetime import datetime, timedelta
 
-        print(f'Hotel {index + 1}:\n\t Name: {name.text.strip()}')
-        print(f'\t Address: {address.text.strip()}')
-        print(f'\t Distance from Center: {distance.text.strip()}')
-        if newToBooking and newToBooking.text == "New to Booking.com": print(f'\t New to Booking.com')
-        print(f'\t Reviews: {numOfReviews.text}') if numOfReviews else print('\t Reviews: No reviews yet')
-        print(f'\t Extras: {extras.text}') if extras else print('\t Extras: No extras')
-        print(f'\t Nights and people: {xNightsAndAdults.text.strip()}') if xNightsAndAdults else print(
-            '\t Nights and people: No info')
-        print(f'\t Price: {price.text.strip()}') if price else print('\t Price: No price')
-        print(f'\t Taxes and charges: {taxesAndCharges.text.strip()}') if taxesAndCharges else print(
-            '\t Taxes and charges: No info')
-        print("----------------------------------------------")
+from datetime import datetime, timedelta
 
 
-if __name__ == '__main__':
-    url = 'https://www.booking.com/searchresults.html?ss=Berlin&ssne=Berlin&ssne_untouched=Berlin&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaGqIAQGYAQ64ARfIAQzYAQHoAQGIAgGoAgO4AoD2taIGwAIB0gIkYjUwNGU2YzctYmIxNS00NDEyLWIwZDMtMjYxN2Y4MDg4YmQ32AIF4AIB&sid=77a53ffe11c95ad4fb890587265110d0&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-1746443&dest_type=city&checkin=2023-05-31&checkout=2023-06-14&group_adults=2&no_rooms=1&group_children=0'
-    url2 = 'https://www.booking.com/searchresults.html?ss=Berlin&ssne=Berlin&ssne_untouched=Berlin&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaGqIAQGYAQ64ARfIAQzYAQHoAQGIAgGoAgO4AoD2taIGwAIB0gIkYjUwNGU2YzctYmIxNS00NDEyLWIwZDMtMjYxN2Y4MDg4YmQ32AIF4AIB&sid=77a53ffe11c95ad4fb890587265110d0&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-1746443&dest_type=city&checkin=2023-05-31&checkout=2023-06-14&group_adults=2&no_rooms=1&group_children=0'
-    # set up selenium driver
-    driver = webdriver.Chrome()
-    driver.get(url)
+def generate_date_tuples(date_str):
+    # Convert date string to datetime object
+    date = datetime.strptime(date_str, "%Y-%m-%d")
 
-    # set implicit wait time to 10 seconds
-    driver.implicitly_wait(10)
+    # Calculate one week and one month offsets
+    one_week_offset = timedelta(weeks=1)
+    one_month_offset = timedelta(days=30)
 
-    # wait until the element with ID "my_element" is present
-    element_present = EC.presence_of_element_located(
-        (By.CLASS_NAME, "a826ba81c4"))  # the first class of the property card
-    WebDriverWait(driver, timeout=10).until(element_present)
+    # Initialize the list of tuples
+    date_tuples = []
 
-    # now that the element is present, get the page source
-    html_source = driver.page_source
+    # Generate tuples for given date + 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append((date_str, next_date))
 
-    # close the browser
-    driver.quit()
+    # Generate tuples for given date + one week and 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + one_week_offset + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append(((date + one_week_offset).strftime("%Y-%m-%d"), next_date))
 
-    # use BeautifulSoup to parse the page source and extract the data you need
-    soup = BeautifulSoup(html_source, "html.parser")
+    # Generate tuples for given date + one month and 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + one_month_offset + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append(((date + one_month_offset).strftime("%Y-%m-%d"), next_date))
 
-    source = requests.get(url)
-    property_cards = soup.find_all('div', {'data-testid': 'property-card'})
+    return date_tuples
 
-    printPropertyCards(property_cards)
+
+date_str = "2023-11-29"
+result = generate_date_tuples(date_str)
+print(result)
--- BeautifulSoupDemo/Tests.py
+++ BeautifulSoupDemo/Tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 import random as ran
 import sys
@@ -5,13 +6,15 @@ import sys
 import codecs
 import requests
 import re
+import os
 from selenium import webdriver
-from selenium.common import NoSuchElementException
+from selenium.common import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 
 def searchDate(checkInDate, checkOutDate):
@@ -47,13 +50,56 @@ def searchDate(checkInDate, checkOutDate):
                                                     and "//*[@class='fc63351294 a822bdf511 e3c025e003 fa565176a8 cfb238afa1 c334e6f658 ae1678b153 c9fa5fc96d be298b15fa']")
             next_month_button.click()
 
-    search_button = driver.find_element(By.XPATH, "//*[@type='submit']"
-                                        and ("//*[@class='fc63351294 a822bdf511 d4b6b7a9e7 cfb238afa1 c938084447 f4605622ad aa11d0d5cd']"
-                                             or "//*[@class='fc63351294 a822bdf511 d4b6b7a9e7 f7db01295e c938084447 f4605622ad c827b27356']"))
+    try:
+        search_button = driver.find_element(By.XPATH, "//*[@type='submit']"
+                                            and "//*[@class='fc63351294 a822bdf511 d4b6b7a9e7 cfb238afa1 c938084447 f4605622ad aa11d0d5cd']")
+
+    except NoSuchElementException:
+        search_button = driver.find_element(By.XPATH, "//*[@type='submit']"
+                                            and "//*[@class='fc63351294 a822bdf511 d4b6b7a9e7 f7db01295e c938084447 f4605622ad c827b27356']")
     search_button.click()
 
+"""
+# Given a date of "yyyy-mm-dd" returns a list of tuples:
+    Example:
+        date_str = '2023-06-14'
+        result = generate_date_tuples(date_str)
+        print(result)
+        # Output: [('2023-06-14', '2023-06-15'), ('2023-06-14', '2023-06-16'), ('2023-06-14', '2023-06-17'), ('2023-06-14', '2023-06-18'),
+        #          ('2023-06-21', '2023-06-15'), ('2023-06-21', '2023-06-16'), ('2023-06-21', '2023-06-17'), ('2023-06-21', '2023-06-18'),
+        #          ('2023-07-14', '2023-06-15'), ('2023-07-14', '2023-06-16'), ('2023-07-14', '2023-06-17'), ('2023-07-14', '2023-06-18')]
 
-url = 'https://www.booking.com/searchresults.html?ss=Berlin&ssne=Berlin&ssne_untouched=Berlin&label=gen173nr-1FCAEoggI46AdIM1gEaGqIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4AoHj0aMGwAIB0gIkNTJkOTAzNjAtYjA4Yy00YzM0LWI4YjgtYWUyMGE2YmY1MGMw2AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=index&dest_id=-1746443&dest_type=city&checkin=2023-05-31&checkout=2023-06-14&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
+    """
+def generate_date_tuples(date_str):
+    # Convert date string to datetime object
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+
+    # Calculate one week and one month offsets
+    one_week_offset = timedelta(weeks=1)
+    one_month_offset = timedelta(days=30)
+
+    # Initialize the list of tuples
+    date_tuples = []
+
+    # Generate tuples for given date + 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append((date_str, next_date))
+
+    # Generate tuples for given date + one week and 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + one_week_offset + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append(((date + one_week_offset).strftime("%Y-%m-%d"), next_date))
+
+    # Generate tuples for given date + one month and 1 to 4 days
+    for i in range(1, 5):
+        next_date = (date + one_month_offset + timedelta(days=i)).strftime("%Y-%m-%d")
+        date_tuples.append(((date + one_month_offset).strftime("%Y-%m-%d"), next_date))
+
+    return date_tuples
+
+
+url = 'https://www.booking.com/searchresults.html?ss=Berlin%2C+Berlin+Federal+State%2C+Germany&ssne=Dafna&ssne_untouched=Dafna&label=gen173nr-1FCAEoggI46AdIM1gEaGqIAQGYATG4ARfIAQzYAQHoAQH4AQOIAgGoAgO4Av6myqQGwAIB0gIkN2NiZTVhMWEtNjdiNy00ODA4LTllNzEtZTQ2ZTVjN2NhYjYz2AIF4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=index&dest_id=-1746443&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=92ee2aff31a3024e&ac_meta=GhA5MmVlMmFmZjMxYTMwMjRlIAAoATICZW46BmJlcmxpbkAASgBQAA%3D%3D&checkin=2023-06-30&checkout=2023-07-27&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
 
 # set up selenium driver
 driver = webdriver.Chrome()
@@ -73,25 +119,55 @@ html_source = driver.page_source
 # use BeautifulSoup to parse the (MAIN PAGE) page source and extract the data you need
 soup = BeautifulSoup(html_source, "html.parser")
 
+# searchDate("2023-07-22", "2023-10-15")
 
-searchDate("2023-07-22", "2023-10-15")
 
 # time.sleep(10)
-file_path = 'C:\\Users\\Yuval\\Desktop\\אקדמיה\\B.sc CS COLMAN\\תכנית מצטיינים\\HotelsHTMLSource'  # Replace with your desired file path
 
 
-# property_cards = soup.find_all('div', {'data-testid': 'property-card'})
-# links = []
-# for card in property_cards:
-#     links.append(card.find('a', {'data-testid': 'title-link'})['href'])
-#
-# i=1
-# for link in links:
-#     driver.get(link)
-#     time.sleep(3)
-#     html_hotel_source = driver.page_source
-#     print(i)
-#     with open(file_path+f'/Berlin_index_{i}_1-2-22_2-2-22.html', 'w', encoding='utf-8') as file:
-#         file.write(html_hotel_source)
-#     i=i+1
+def save_data_from_search_results(date):
+    file_path = r"C:\Users\liavb\Desktop\SortThingsML\data" # Replace with your desired file path
+    folder_name = f"{date[0]}___{date[1]}"
+    folder_path = os.path.join(file_path, folder_name)
+    os.makedirs(folder_path, exist_ok=True)
 
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    property_cards = soup.find_all('div', {'data-testid': 'property-card'})
+    links = []
+    for card in property_cards:
+        links.append(card.find('a', {'data-testid': 'title-link'})['href'])
+
+    i = 1
+    for link in links[:2]:
+        driver.get(link)
+        time.sleep(1)
+        html_hotel_source = driver.page_source
+        file_name = f'Berlin_index_{i}_{date[0]}_{date[1]}.html'
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(html_hotel_source)
+        i += 1
+
+
+def scrape():
+    today = datetime.today()
+    date_string = today.strftime("%Y-%m-%d")
+    dates = generate_date_tuples(date_string)
+
+    for date in dates:
+        # Go back to the original page we started searching from, removes the need of handling multiple cases for random stuff that happens...
+        driver.get(url)
+        pop_up = True
+        while pop_up:
+            try:
+                searchDate(date[0], date[1])
+                save_data_from_search_results(date)
+                pop_up = False
+            except ElementClickInterceptedException:
+                button_xpath = '//button[@aria-label="Dismiss sign-in info."]'
+                button = driver.find_element(By.XPATH, button_xpath)
+                button.click()
+                print('pop up but continued')
+
+
+scrape()
